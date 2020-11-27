bc7ff05b9aa55cd039f973a69b8873890326de21
Damiano Petrungaro <damiano.petrungaro@messagebird.com>
2020-11-27 20:36:23 +0100

    feat: init project using maven

---
+++ src/test/java/com/damianopetrungaro/app/AppTest.java
@@ -0,0 +1,20 @@
+package test.java.com.damianopetrungaro.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
